07c219ec87b16f71127751d20e376b928b9ce617
Jet <jx2421533662@gmail.com>
2021-10-02 20:07:45 +1000

    updated regex

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from Database.DB import *
 import re
 
-if not bool(re.match('^(\d{4})[/.-](\d{1,2})[/.-](\d{1,2})$',"2021/03/20")):
+if not bool(re.match('^(\d{4})[/](\d{1,2})[/](\d{1,2})$',"1999/2/3")):
     print("please enter valid date, format : YYYY/MM/DD")
 
 
