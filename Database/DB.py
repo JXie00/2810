c045d2df125a7df0ffe16301efb245bdafeb164c
Jet <jx2421533662@gmail.com>
2021-10-02 13:01:56 +1000

    added DB querys

--- Database/DB.py
+++ Database/DB.py
@@ -1,10 +1,40 @@
 import sqlite3
+
 con = sqlite3.connect("../assignment.db")
+cur = con.cursor()
+
+cur.execute("update assignment set ACCIDENT_DATE = REPLACE(ACCIDENT_DATE,'/','')")
+cur.execute(
+    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 4, 4) || '' || substr(ACCIDENT_DATE, 3, 1) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 7")
+cur.execute(
+    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 3, 4) || '' || substr(ACCIDENT_DATE, 2, 1) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 6")
+cur.execute(
+    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 5, 4) || '' || substr(ACCIDENT_DATE, 3, 2) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 8")
 
 
-def FetchInitData(type) :
-    cur = con.cursor()
-    cur.execute("select * from assignment order by {} asc limit 50".format(type))
-    res = cur.fetchall()
-    return res
+def fetchInitData(type, increment=1):
+    return cur.execute(
+        "select accident_DATE from assignment order by {} asc limit ({}-1)*50, 50".format(type, increment)).fetchall()
+
+
+def getDataByDate(startDate, endDate):
+    startDate = startDate.replace("/", "")
+    endDate = endDate.replace("/", "")
+    return cur.execute(
+        "select * from assignment where ACCIDENT_DATE between '{}' and '{}' order by ACCIDENT_DATE asc".format(
+            startDate, endDate)).fetchall()
+
+
+def getDataByType(startDate, endDate, type="Collision"):
+    startDate = startDate.replace("/", "")
+    endDate = endDate.replace("/", "")
+    return cur.execute(
+        "select ACCIDENT_TYPE from assignment where ACCIDENT_DATE between '{}' and '{}' and ACCIDENT_TYPE like '%{}%' order by ACCIDENT_DATE asc".format(
+            startDate, endDate, type)).fetchall()
+
+
+def alcoholImpact():
+    totalAccident = cur.execute("SELECT COUNT(*) FROM assignment").fetchall()
+    alcoholAccident = cur.execute("select COUNT(*) from assignment where ALCOHOL_RELATED = 'Yes'").fetchall()
+    return alcoholAccident[0][0] / totalAccident[0][0]
 
--- Database/DBinit.py
+++ Database/DBinit.py
@@ -2,10 +2,10 @@ import sqlite3
 import pandas
 
 con = sqlite3.connect("../assignment.db")
-df = pandas.read_csv("Crash Statistics Victoria.csv")
-df.to_sql("assignment", con, if_exists='append', index=False)
+df = pandas.read_csv("../Crash Statistics Victoria.csv")
+df.to_sql("assignment", con, if_exists='fail', index=False)
 
 cur = con.cursor()
-cur.execute("select OBJECTID from assignment")
 
-res = cur.fetchall()
+cur.execute("select ACCIDENT_DATE from assignment").fetchall()
+
