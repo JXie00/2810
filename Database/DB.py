3ccdecf2508aceb0e474f497f473978793e8ad04
Jet <jx2421533662@gmail.com>
2021-10-02 19:02:35 +1000

    added regex for date

--- Database/DB.py
+++ Database/DB.py
@@ -3,19 +3,9 @@ import sqlite3
 con = sqlite3.connect("../assignment.db")
 cur = con.cursor()
 
-cur.execute("update assignment set ACCIDENT_DATE = REPLACE(ACCIDENT_DATE,'/','')")
-cur.execute(
-    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 4, 4) || '' || substr(ACCIDENT_DATE, 3, 1) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 7")
-cur.execute(
-    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 3, 4) || '' || substr(ACCIDENT_DATE, 2, 1) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 6")
-cur.execute(
-    "update assignment set ACCIDENT_DATE = substr(ACCIDENT_DATE, 5, 4) || '' || substr(ACCIDENT_DATE, 3, 2) || '' || substr(ACCIDENT_DATE, 1, 2) where length(ACCIDENT_DATE) = 8")
-cur.execute("update assignment set ACCIDENT_TIME = substr(ACCIDENT_TIME,1,2)")
-
-
 def fetchInitData(type, increment=1):
     return cur.execute(
-        "select accident_DATE from assignment order by {} asc limit ({}-1)*50, 50".format(type, increment)).fetchall()
+        "select * from assignment order by {} asc limit ({}-1)*50, 50".format(type, increment)).fetchall()
 
 
 def hourlyAverageAccident(startDate, endDate):
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from assignment.Database.DBinit import *
-from assignment.Database.DB import *
+from Database.DB import *
+import re
+
+if not bool(re.match('^(\d{4})[/.-](\d{1,2})[/.-](\d{1,2})$',"2021/03/20")):
+    print("please enter valid date, format : YYYY/MM/DD")
 
 
 
 
+print(fetchInitData("OBJECTID"))
 
 
 
