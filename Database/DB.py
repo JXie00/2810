3fc1df2cb7286f3b8d1c362ce060abd32f1a68f2
Jet <jx2421533662@gmail.com>
2021-10-07 11:48:54 +1000

    added function to get all accident types

--- Database/DB.py
+++ Database/DB.py
@@ -39,7 +39,8 @@ def alcoholImpact():
     return alcoholAccident[0][0] / totalAccident[0][0]
 
 
-
-
 def LGARate():
     return cur.execute("select count(LGA_NAME_ALL), LGA_NAME_ALL from assignment group by LGA_NAME_ALL").fetchall()
+
+def getAccidentType():
+    return cur.execute("select distinct ACCIDENT_TYPE from assignment").fetchall()
--- app.py
+++ app.py
@@ -4,6 +4,3 @@ import re
 if not bool(re.match('^(\d{4})[/](\d{1,2})[/](\d{1,2})$',"1999/2/3")):
     print("please enter valid date, format : YYYY/MM/DD")
 
-
-
-
